6d18cff8c1819e567277192d1d21a7728d8e7f9a
Her-reH <herywijaya2709@gmail.com>
2024-11-27 16:52:43 +0700

    Login

---
+++ latlogin/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:latlogin/login.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aplikasi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
