8638f815ae1e471e9d2f24b14ba4086fd2face4e
SnapCoder4 <valwagiraldy24@gmail.com>
2024-12-11 03:35:08 +0700

    Update dashboard_page.dart

--- dashboard_page.dart
+++ dashboard_page.dart
@@ -22,7 +22,7 @@ class _DashboardPageState extends State<DashboardPage> {
     try {
       final response = await http.get(
         Uri.parse(
-            'http://192.168.1.2/lat_login/get_products.php'), // Ganti dengan URL API Anda
+            '/lat_login/get_products.php'), // Ganti dengan URL API Anda
       );
 
       if (response.statusCode == 200) {
