79bcc2466a236027239d9cbfaadd9aa906ead10e
SnapCoder4 <valwagiraldy24@gmail.com>
2024-12-13 17:21:29 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,24 @@
-import 'login.dart';
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  // Fungsi untuk toggle tema
+  void toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,8 +26,17 @@ class MyApp extends StatelessWidget {
       title: 'Aplikasi',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        brightness: Brightness.light,
       ),
-      home: LoginPage(),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      themeMode: _themeMode, // Aktifkan tema berdasarkan mode
+      home: LoginPage(
+        toggleTheme: toggleTheme, // Berikan fungsi toggleTheme
+        isDarkMode: _themeMode == ThemeMode.dark, // Indikator dark mode
+      ),
     );
   }
 }
